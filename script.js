cae7eaadbcff411c2c06eeaf0c7f11f0786d1e1d
Gabriella Simon <gb@gabriellasimon.me>
2016-10-05 10:33:48 +0200

    New quote on refresh

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $.getJSON('http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?', function(data) {
+    var quoteText = data.quoteText;
+    var quoteAuthor = data.quoteAuthor;
+    $('#quote').text(quoteText);
+    $('#author').text(quoteAuthor);
+  });
+  $()
+    quoteText = "";
+    quoteAuthor = "";
+});
