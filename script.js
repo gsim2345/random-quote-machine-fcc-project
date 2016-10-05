4825fd55185910250a386991dc01d85af9c69d43
Gabriella Simon <gb@gabriellasimon.me>
2016-10-05 12:30:30 +0200

    Add alert with materialize toast component

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+  // getting the first quote with getJSON
   $.getJSON('http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?', function(data) {
     var quoteText = data.quoteText;
     var quoteAuthor = data.quoteAuthor;
@@ -7,6 +8,7 @@ $(document).ready(function() {
     $('#author').text(quoteAuthor);
   });
 
+  // getting a new quote on click
   $('#newQuoteButton').on('click', function() {
     //$('#quote').text("");
     //$('#author').text("");
@@ -18,13 +20,16 @@ $(document).ready(function() {
     });
   });
 
-  $('#tweetButton').on('click', function() {
-
+  // tweeting the quote on click
+  $('#tweetButton').on('click', function(event) {
       var textToTweet = $('#quote').text() + $('#author').text();
       console.log(textToTweet.length);
+      // if tweet is longer than 140 chars, alert sent with materialize toast component.
       if (textToTweet.length > 140) {
-
+        event.preventDefault();
+        Materialize.toast('Your tweet is longer than 140 characters !', 6000);
       } else {
+      // if it's < 140 chars, tweeting out.  
         $('#tweetButton').attr('href', 'https://twitter.com/intent/tweet?text=' + textToTweet + '');
       }
 
