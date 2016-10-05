3c87a14f0d873b1fdd1196198a2eb3ee6ea1ffd3
Gabriella Simon <gb@gabriellasimon.me>
2016-10-05 11:09:04 +0200

    New quotes work on click

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
+
   $.getJSON('http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?', function(data) {
     var quoteText = data.quoteText;
     var quoteAuthor = data.quoteAuthor;
     $('#quote').text(quoteText);
     $('#author').text(quoteAuthor);
   });
-  $()
-    quoteText = "";
-    quoteAuthor = "";
+
+  $('#newQuoteButton').on('click', function() {
+    //$('#quote').text("");
+    //$('#author').text("");
+    $.getJSON('http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?', function(data) {
+      var quoteText = data.quoteText;
+      var quoteAuthor = data.quoteAuthor;
+      $('#quote').text(quoteText);
+      $('#author').text(quoteAuthor);
+    });
+  });
+
 });
