251fe06d96687ebeb420d72b0b8784d8db283779
Gabriella Simon <gb@gabriellasimon.me>
2016-11-03 21:08:04 +0100

    footer fixed and api test

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 $(document).ready(function() {
 
-
-  //function that changes the background color randomly.
+  /** function that changes the background color randomly. */
   var colorize = function() {
     var colors = ['#258EA6', '#759FBC', '#90C3C8', '#B9B8D3', '#59A96A', '#9BDEAC', '#B4E7CE', '#AC80A0', '#DCC9B6', '#ABC4AB', '#68C5DB'];
     var number = Math.floor(Math.random() * 10);
     $('body').css('background-color', colors[number]);
     $('button').css('background-color', colors[number]);
   };
-
-  //function that gets the data from the API, and show on the page
+  /** function that gets the data from the API, and show on the page */
   var getData = function() {
     $('#quote').html('<img src="status.gif" alt="preloader gif">');
     $('#author').html('');
     $('.quoteDislay').css('background-color', 'white');
-    var quoteURL = "https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?";
+    var quoteURL = "http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?";
     $.getJSON(quoteURL, {
       format: "jsonp"
     })
@@ -22,7 +20,7 @@ $(document).ready(function() {
       $('.quoteDislay').css('background-color', 'rgb(248, 248, 248)');
       var quoteText = data.quoteText;
       var quoteAuthor = data.quoteAuthor;
-      // inserting the data on the page
+      /** inserting the data on the page */
       $('#quote').text('" ' + quoteText + '"');
       if (quoteAuthor.length === 0) {
         $('#author').text('- Anonymous');
